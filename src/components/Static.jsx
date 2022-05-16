e2049a93dbd4263f2bb9b8205ea9ae917ebabacd
Danny12244 <pradeep.py725@gmail.com>
2022-05-16 23:28:29 +0530

    changed

---
+++ src/components/Static.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function Card() {
+    return (<><h1>Mobile Operating System</h1><ul>
+        <li>Android</li>
+        <li>BlackBerry</li>
+        <li>iPhone</li>
+        <li>Windows</li>
+    </ul>
+    <h1>Mobile Manufacturers</h1>
+    <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Micromax</li>
+        <li type="circle">Apple</li>
+    </ul>
+    </>);
+}
+
+export default Card;
